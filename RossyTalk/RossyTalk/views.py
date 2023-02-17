94cc54d0506a41632b398a3c15312190932a83f0
Billyz <562830882@qq.com>
2023-02-17 18:16:46 +0800

    Add django project

--- RossyTalk/RossyTalk/views.py
+++ RossyTalk/RossyTalk/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
 
-def hello(request):
-    return HttpResponse('hello there')
+def rossy(request):
+    context = {}
+    context['hello'] = 'Hello Rossy'
+    return
